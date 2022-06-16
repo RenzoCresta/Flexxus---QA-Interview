8db973fb8fb7e1e9d48559f7a1ccee1750232418
renzo.cresta <renzo_cresta@gmail.com>
2022-06-16 02:44:19 -0300

    DONE.

--- Flexxus - SQL Exercises.sql
+++ Flexxus - SQL Exercises.sql
@@ -20,37 +20,37 @@ inner join EMPLEADOS on DEPARTAMENTOS.ID = EMPLEADOS.DEPARTAMENTO_ID
 inner join PUESTOS on EMPLEADOS.PUESTO_ID = PUESTOS.ID
 where LOCALIDADES.LOCALIDAD = 'CARLOS PAZ';
 
-########## Seleccionar el nombre, sueldo y localidad donde trabajan de los empleados que tengan un sueldo entre 10000 y 13000.##########
+########## Seleccionar el nombre, sueldo y localidad donde trabajan de los empleados que tengan un sueldo entre 10000 y 13000. ##########
 select EMPLEADOS.NOMBRES, EMPLEADOS.SUELDO, LOCALIDADES.LOCALIDAD
 from DEPARTAMENTOS inner join EMPLEADOS on DEPARTAMENTOS.ID = EMPLEADOS.DEPARTAMENTO_ID
 inner join LOCALIDADES on DEPARTAMENTOS.LOCALIDAD_ID = LOCALIDADES.ID
 where EMPLEADOS.SUELDO >= 10000 and EMPLEADOS.SUELDO <= 13000;
 
-########## Visualizar los departamentos con más de 5 empleados.##########
+########## Visualizar los departamentos con más de 5 empleados. ##########
 select DEPARTAMENTOS.DENOMINACION, count(EMPLEADOS.DEPARTAMENTO_ID) as 'PERSONAL'
 from EMPLEADOS inner join DEPARTAMENTOS on DEPARTAMENTOS.ID = EMPLEADOS.DEPARTAMENTO_ID
 group by DEPARTAMENTOS.DENOMINACION
 having count(EMPLEADOS.DEPARTAMENTO_ID) > 5;
 
-########## Nombre de los empleados que trabajan en Córdoba y cuyo puesto sea ‘Analista’ o ‘Programador’.##########
+########## Nombre de los empleados que trabajan en Córdoba y cuyo puesto sea ‘Analista’ o ‘Programador’. ##########
 select EMPLEADOS.NOMBRES
 from DEPARTAMENTOS inner join LOCALIDADES on DEPARTAMENTOS.LOCALIDAD_ID = LOCALIDADES.ID
 inner join EMPLEADOS on DEPARTAMENTOS.ID = EMPLEADOS.DEPARTAMENTO_ID
 inner join PUESTOS on EMPLEADOS.PUESTO_ID = PUESTOS.ID
 where LOCALIDAD = 'CORDOBA' and (PUESTO = 'ANALISTA' or PUESTO = 'PROGRAMADOR');
 
-##########Calcula el sueldo medio de todos los empleados.##########
+########## Calcula el sueldo medio de todos los empleados. ##########
 select avg(SUELDO) from EMPLEADOS;
 
-##########¿Cuál es el máximo sueldo de los empleados del departamento 10?##########
+########## ¿Cuál es el máximo sueldo de los empleados del departamento 10? ##########
 select max(SUELDO) from EMPLEADOS where DEPARTAMENTO_ID = 10;
 
-##########Calcula el sueldo mínimo de los empleados del departamento ‘Soporte’.##########
+########## Calcula el sueldo mínimo de los empleados del departamento ‘Soporte’. ##########
 select min(SUELDO)
 from EMPLEADOS inner join DEPARTAMENTOS on DEPARTAMENTOS.ID = EMPLEADOS.DEPARTAMENTO_ID
 where DEPARTAMENTOS.DENOMINACION = 'SOPORTE';
 
-##########Para cada puesto obtener la suma de sueldos.##########
+########## Para cada puesto obtener la suma de sueldos. ##########
 select PUESTOS.PUESTO, sum(EMPLEADOS.SUELDO) as 'TOTAL SALARIOS'
 from EMPLEADOS inner join PUESTOS on PUESTOS.ID = EMPLEADOS.PUESTO_ID
 group by PUESTOS.PUESTO;
